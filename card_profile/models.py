4c0886f6c4993814dba94b12411c1890ab8ba36b
Risalat Shahriar <risalat.shahriar2810@gmail.com>
2023-03-07 19:12:45 +0600

    files added

--- card_profile/models.py
+++ card_profile/models.py
@@ -3,14 +3,13 @@ from django.db import models
 # Create your models here.
 class CardUser(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
-    bio = models.CharField(max_length=400, null=True, blank=True)
+    designation = models.CharField(max_length=400, null=True, blank=True)
     about = models.CharField(max_length=500, null=True, blank=True)
     phone_number = models.CharField(max_length=11, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     address = models.CharField(max_length=400, null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/')
     cover_picture = models.ImageField(upload_to='cover_pictures')
-    card_linked = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- card_profile/views.py
+++ card_profile/views.py
@@ -7,10 +7,26 @@ def view_profile(request, id):
     data = models.CardUser.objects.get(id=id)
     return render(request, 'User/userProfile.html', 
       {
-        'data': data
+        'data': data,
+        'auth': request.user.is_authenticated
       }
     )
 
 @login_required(login_url='login')
 def edit_profile(request, id):
-    return render(request, 'User/editProfile.html')
+    data = models.CardUser.objects.get(id=id)
+    if request.method == 'POST':
+        data.name = request.POST['name']
+        data.designation = request.POST['designation']
+        data.about = request.POST['about']
+        data.phone_number = request.POST['phone_number']
+        data.email = request.POST['email']
+        data.address = request.POST['address']
+
+        data.save()
+
+    return render(request, 'User/editProfile.html', 
+      {
+        'data': data
+      }
+    )
