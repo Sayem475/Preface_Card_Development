88cb19fdbfdf813553b9e11651f7c6a2562b56a6
Risalat Shahriar <risalat.shahriar2810@gmail.com>
2023-03-07 19:43:37 +0600

    auth added

--- profile_login/urls.py
+++ profile_login/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.profile_login, name='login'),
+    path('logout', views.profile_logout, name='logout')
 ]
--- profile_login/views.py
+++ profile_login/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login,logout
 
 # Create your views here.
 def profile_login(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['name'], password=request.POST['pass'])
+        if user is not None:
+            login(request, user)
+            return redirect('editProfile', user.id)
+        print(user)
     return render(request, 'Auth/userlogin.html')
+
+def profile_logout(request):
+    logout(request)
+    return redirect('login')
