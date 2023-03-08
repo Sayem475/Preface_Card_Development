5a85fd72c93c6055ca11893b93046f6469e7624f
Md Sayem Uddin <sayemuddin471@gmail.com>
2023-03-08 21:49:33 +0600

    card on board

--- card_profile/urls.py
+++ card_profile/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>', views.view_profile, name='view_profile'),
-    path('edit/<int:id>', views.edit_profile, name='editProfile')
+    path('<int:id>', views.view_profile, name='viewProfile'),
+    path('edit/<int:id>', views.edit_profile, name='editProfile'),
+    path('admin/onboard', views.profile_onboard_log, name='onboardLog'),
+    path('admin/onboard/<int:id>', views.profile_onboard, name='onBoard')
 ]
